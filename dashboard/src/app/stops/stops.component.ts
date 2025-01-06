2f4f6a06e38e70d4181575b2c5104ae7b9bc9486
DohrMelanie <m.dohr@students.htl-leonding.ac.at>
2025-01-06 16:33:46 +0100

    KAN-114 added stops component

---
+++ dashboard/src/app/stops/stops.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stops',
+  standalone: true,
+  imports: [],
+  templateUrl: './stops.component.html',
+  styleUrl: './stops.component.css'
+})
+export class StopsComponent {
+
+}
