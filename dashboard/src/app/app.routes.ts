ff38d9bd8976e753f47fab22abc3732d8355d6b8
HaasLuca <l.haas@students.htl-leonding.ac.at>
2025-01-06 15:51:31 +0100

    KAN-80 Removed test components

--- dashboard/src/app/app.routes.ts
+++ dashboard/src/app/app.routes.ts
@@ -3,12 +3,8 @@ import { LoginComponent } from './login/login.component';
 import { StopGroupsComponent } from './stopgroups/stopgroups.component';
 import { DivisionsListComponent } from './divisions-list/divisions-list.component';
 import { DivisionDetailsComponent } from './division-details/division-details.component';
-import {TestComponent} from "./test/test.component";
-import {SecondTestComponent} from "./second-test/second-test.component";
 
 export const routes: Routes = [
-  { path: 'test', component: TestComponent },
-  { path: 'second', component: SecondTestComponent },
   { path: 'login', component: LoginComponent },
   { path: 'stopgroups', component: StopGroupsComponent },
   { path: 'divisions', component: DivisionsListComponent },
