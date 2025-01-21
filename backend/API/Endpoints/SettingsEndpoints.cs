5fce973320b4279027b2daf66edcb9ce4b292172
DohrMelanie <m.dohr@students.htl-leonding.ac.at>
2025-01-21 21:30:32 +0100

    added check of api key before login

--- backend/API/Endpoints/SettingsEndpoints.cs
+++ backend/API/Endpoints/SettingsEndpoints.cs
@@ -1,5 +1,7 @@
 using Database;
+using Database.Entities;
 using Database.Repository;
+using Microsoft.EntityFrameworkCore;
 using System.IO;
 
 namespace API.Endpoints;
@@ -8,8 +10,9 @@ public static class SettingsEndpoints
 {
     public static void MapSettingsEndpoints(this IEndpointRouteBuilder app)
     {
-        var group = app.MapGroup("v1/api");
-        group.MapGet("resetDB", ResetDataBase);
+        var group = app.MapGroup("v1");
+        group.MapGet("api/resetDB", ResetDataBase);
+        group.MapGet("keyExists", ApiKeyExists);
     }
 
     public static async Task ResetDataBase(TadeoTDbContext context)
@@ -17,7 +20,13 @@ public static class SettingsEndpoints
         new BadHttpRequestException("Not implemented yet");
     }
     
-    
-    
-    
+    public static async Task<IResult> ApiKeyExists(TadeoTDbContext context, string key)
+    {
+        var apiKey = await context.APIKeys.FirstOrDefaultAsync(x => x.APIKeyValue == key);
+        if (apiKey == null)
+        {
+            return Results.NotFound($"Apikey not valid!");
+        }
+        return Results.Ok();
+    }
 }
