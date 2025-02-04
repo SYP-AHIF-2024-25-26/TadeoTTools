62f1fd814cb7db9cdd6df78074b4063660a68a72
DohrMelanie <m.dohr@students.htl-leonding.ac.at>
2025-02-04 15:34:49 +0100

    added demo

--- dashboard/src/app/app.config.ts
+++ dashboard/src/app/app.config.ts
@@ -4,16 +4,48 @@ import {
   provideZoneChangeDetection,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
 import { routes } from './app.routes';
 import { environment } from '../environments/environment';
 export const BASE_URL = new InjectionToken<string>('BaseUrl');
 
+import {
+  createInterceptorCondition,
+  INCLUDE_BEARER_TOKEN_INTERCEPTOR_CONFIG,
+  IncludeBearerTokenCondition, includeBearerTokenInterceptor,
+  provideKeycloak
+} from "keycloak-angular";
+
+const authTokenCondition = createInterceptorCondition<IncludeBearerTokenCondition>({
+  urlPattern: /^(http:\/\/localhost:5050)(\/.*)?$/i
+});
+const keycloakProvider = provideKeycloak({
+  config: {
+    url: 'https://auth.htl-leonding.ac.at', // URL of the Keycloak server
+    realm: 'htlleonding', // Realm to be used in Keycloak
+    clientId: 'htlleonding-service' // Client ID for the application in Keycloak,
+  },
+  initOptions: {
+    onLoad: 'check-sso', // Action to take on load
+    //enableLogging: true, // Enables logging
+    // IMPORTANT: implicit flow is no longer recommended, but using standard flow leads to a 401 at the keycloak server
+    // when retrieving the token with the access code - we leave it like this for the moment until a solution is found
+    flow: 'implicit'
+  },
+  providers: [
+    {
+      provide: INCLUDE_BEARER_TOKEN_INTERCEPTOR_CONFIG,
+      useValue: [authTokenCondition] // Specify conditions for adding the Bearer token
+    }
+  ]
+});
+
 export const appConfig: ApplicationConfig = {
   providers: [
+    keycloakProvider,
     provideZoneChangeDetection({ eventCoalescing: true }),
     { provide: BASE_URL, useValue: environment.apiBaseUrl },
     provideRouter(routes),
-    provideHttpClient(withFetch())
+    provideHttpClient(withFetch(), withInterceptors([includeBearerTokenInterceptor])),
   ],
 };
--- dashboard/src/app/login/login.component.ts
+++ dashboard/src/app/login/login.component.ts
@@ -1,7 +1,8 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, signal, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { SettingsService } from '../settings.service';
+import Keycloak from "keycloak-js";
 
 @Component({
   selector: 'app-login',
@@ -13,20 +14,27 @@ import { SettingsService } from '../settings.service';
 export class LoginComponent {
   private service: SettingsService = inject(SettingsService);
 
-  apiKey = signal<string>('');
+  private readonly keycloak = inject(Keycloak);
+  protected isLoggedIn = this.keycloak.authenticated ?? false;
 
-  constructor(private router: Router) {
-    this.apiKey.set(localStorage.getItem('API_KEY') || '');
+  public ngOnInit(): void {
+    if (this.keycloak.token) {
+      console.log(this.keycloak.token);
+    }
   }
 
-  async saveApiKey() {
-    if (this.apiKey() !== '') {
-      localStorage.setItem('API_KEY', this.apiKey());
-      if (await this.service.checkApiKeyExists(this.apiKey())) {
-        this.router.navigate(['/stopgroups']);
-      } else {
-        alert('Invalid API key');
-      }
+  public async login(): Promise<void> {
+    if (this.isLoggedIn) {
+      return;
     }
+    await this.keycloak.login();
+
+  }
+
+  public async logout(): Promise<void> {
+    if (!this.isLoggedIn) {
+      return;
+    }
+    await this.keycloak.logout();
   }
 }
