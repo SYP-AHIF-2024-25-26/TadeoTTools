e9cfeeb90ba27ea5eefdc04dab5c4fb8034d444e
HuberAndreas1 <a.huber1@students.htl-leonding.ac.at>
2025-02-06 11:51:16 +0100

    init

--- frontend/src/app/about-page/about-page.component.ts
+++ frontend/src/app/about-page/about-page.component.ts
@@ -1,12 +1,19 @@
-import { Component, signal, WritableSignal } from '@angular/core';
+import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { HeaderComponent } from '../header/header.component';
 
 @Component({
+<<<<<<< Updated upstream
   selector: 'app-about-page',
   standalone: true,
   imports: [NavbarComponent, HeaderComponent],
   templateUrl: './about-page.component.html',
   styleUrl: './about-page.component.css',
+=======
+    selector: 'app-about-page',
+    imports: [NavbarComponent, HeaderComponent],
+    templateUrl: './about-page.component.html',
+    styleUrl: './about-page.component.css'
+>>>>>>> Stashed changes
 })
 export class AboutPageComponent {}
