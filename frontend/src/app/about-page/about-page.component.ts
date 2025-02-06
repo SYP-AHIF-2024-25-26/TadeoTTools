c4179566fa63c8e8c298caffb6b0cc2f75e5bc48
HuberAndreas1 <a.huber1@students.htl-leonding.ac.at>
2025-02-06 11:53:58 +0100

    fix merge conflicts

--- frontend/src/app/about-page/about-page.component.ts
+++ frontend/src/app/about-page/about-page.component.ts
@@ -3,17 +3,9 @@ import { NavbarComponent } from '../navbar/navbar.component';
 import { HeaderComponent } from '../header/header.component';
 
 @Component({
-<<<<<<< Updated upstream
-  selector: 'app-about-page',
-  standalone: true,
-  imports: [NavbarComponent, HeaderComponent],
-  templateUrl: './about-page.component.html',
-  styleUrl: './about-page.component.css',
-=======
     selector: 'app-about-page',
     imports: [NavbarComponent, HeaderComponent],
     templateUrl: './about-page.component.html',
     styleUrl: './about-page.component.css'
->>>>>>> Stashed changes
 })
 export class AboutPageComponent {}
