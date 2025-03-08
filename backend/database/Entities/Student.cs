a9c60717e087f2aaae0d1bf221c3fd5ec139645a
HuberAndreas1 <a.huber1@students.htl-leonding.ac.at>
2025-03-08 12:43:20 +0100

    KAN-144: Added Student and Teacher Entities

---
+++ backend/database/Entities/Student.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Database.Entities;
+
+public class Student
+{
+    [Key]
+    public required string EdufsUsername { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string StudentClass { get; set; } = string.Empty;
+    public string Department { get; set; } = string.Empty;
+    public List<Stop> Stops { get; set; } = [];
+}
