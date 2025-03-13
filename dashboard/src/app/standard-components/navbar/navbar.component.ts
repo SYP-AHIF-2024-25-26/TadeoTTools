b3b53bb32615deb9d7464ffe2c35f7f8cda160a9
HuberAndreas1 <a.huber1@students.htl-leonding.ac.at>
2025-03-13 11:01:30 +0100

    Added Student and Teacher View for Admin

--- dashboard/src/app/standard-components/navbar/navbar.component.ts
+++ dashboard/src/app/standard-components/navbar/navbar.component.ts
@@ -1,19 +1,29 @@
-import { Component, inject } from '@angular/core';
-import { Router, RouterModule } from '@angular/router';
+import {Component, inject, signal} from '@angular/core';
+import {Router, RouterModule} from '@angular/router';
 import Keycloak from 'keycloak-js';
+import {AdminDropdownComponent} from "../admin-dropdown/admin-dropdown.component";
+import {LoginService} from "../../login.service";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, AdminDropdownComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
   private readonly keycloak = inject(Keycloak);
+  private readonly service = inject(LoginService);
   private readonly router = inject(Router);
+  protected isAdmin = signal(false);
 
   async logout() {
     await this.keycloak.logout({ redirectUri: window.location.origin + '/login' });
+  }
+
+  async ngOnInit() {
+    const response = await this.service.performCall('is-admin');
+
+    this.isAdmin.set(response.includes('admin'));
   }
 }
