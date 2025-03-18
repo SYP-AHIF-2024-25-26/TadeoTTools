d85145f5bfc30c810bf309a7c7ed84cea82dffef
HaasLuca <l.haas@students.htl-leonding.ac.at>
2025-03-18 16:04:15 +0100

    KAN-148 Added TeacherAssignment Model

---
+++ backend/database/Entities/TeacherAssignment.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Database.Entities;
+
+public class TeacherAssignment
+{
+    [Key]
+    public int Id { get; set; }
+    public int TeacherId { get; set; }
+    public Teacher? Teacher { get; set; }
+    public int StopId { get; set; }
+    public Stop? Stop { get; set; }
+    public Status Status { get; set; }
+}
