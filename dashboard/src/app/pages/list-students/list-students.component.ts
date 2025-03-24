34effb7afe33596400fa8a4b7e70743851744911
DohrMelanie <m.dohr@students.htl-leonding.ac.at>
2025-03-24 08:02:41 +0100

    KAN-142 fixed rejected

--- dashboard/src/app/pages/list-students/list-students.component.ts
+++ dashboard/src/app/pages/list-students/list-students.component.ts
@@ -28,32 +28,38 @@ export class ListStudentsComponent implements OnInit {
 
   processStudents(): void {
     // all where there is more than one assignment and at least one is pending
-    this.conflicts.set(this.students().filter(
-      (s) =>
-        s.studentAssignments.filter((a) => a.status === Status.Pending
-      ).length >= 1 && s.studentAssignments.length > 1
-    ));
+    this.conflicts.set(
+      this.students().filter(
+        (s) =>
+          s.studentAssignments.filter((a) => a.status === Status.Pending)
+            .length >= 1 && s.studentAssignments.length > 1
+      )
+    );
     // all where there is only one assingment and that one is pending
-    this.singleAssignments.set(this.students().filter(
-      (s) =>
-        s.studentAssignments.length === 1 &&
-        s.studentAssignments[0].status === Status.Pending
-    ));
+    this.singleAssignments.set(
+      this.students().filter(
+        (s) =>
+          s.studentAssignments.length === 1 &&
+          s.studentAssignments[0].status === Status.Pending
+      )
+    );
     // all where there are no assignments
-    this.noAssignments.set(this.students().filter(
-      (s) => s.studentAssignments.length === 0
-    ));
+    this.noAssignments.set(
+      this.students().filter((s) => s.studentAssignments.length === 0)
+    );
   }
 
   approveStudent(student: Student, index: number): void {
     student.studentAssignments[index].status = Status.Accepted;
-    console.log(student);
+    this.updateStudent(student);
+  }
+
+  undoStatus(student: Student, index: number): void {
+    student.studentAssignments[index].status = Status.Pending;
     this.updateStudent(student);
   }
 
   rejectStudent(student: Student, index: number): void {
-    console.log(student);
-    console.log(index);
     student.studentAssignments[index].status = Status.Declined;
     this.updateStudent(student);
   }
