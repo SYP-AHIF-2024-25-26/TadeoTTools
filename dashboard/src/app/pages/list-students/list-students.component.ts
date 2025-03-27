3a4521514dc5ee33c25760d66af24fed0d3bb19f
HaasLuca <l.haas@students.htl-leonding.ac.at>
2025-03-27 14:46:43 +0100

    KAN-150 updated list-students page to to use studentStore

--- dashboard/src/app/pages/list-students/list-students.component.ts
+++ dashboard/src/app/pages/list-students/list-students.component.ts
@@ -1,81 +1,49 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
-import { StudentService } from '../../student.service';
-import { Status, Student, StudentAssignment } from '../../types';
+import {Component, computed, inject} from '@angular/core';
+import {Status, Student} from '../../types';
+import {StudentStore} from "../../store/student.store";
 
 @Component({
   selector: 'app-list-students',
   imports: [],
   templateUrl: './list-students.component.html',
+  standalone: true
 })
-export class ListStudentsComponent implements OnInit {
-  private service: StudentService = inject(StudentService);
+export class ListStudentsComponent {
+  private studentStore = inject(StudentStore);
 
-  students = signal<Student[]>([]);
-  conflicts = signal<Student[]>([]);
-  singleAssignments = signal<Student[]>([]);
-  noAssignments = signal<Student[]>([]);
-  selectedAssignments: Map<string, number> = new Map();
-  approvedSingles: Set<string> = new Set();
+  // all where there is more than one assignment and at least one is pending
+  conflicts = computed(() =>
+    this.studentStore.students().filter(
+      (s) =>
+        s.studentAssignments.some((a) => a.status === Status.Pending)
+        && s.studentAssignments.length > 1
+    )
+  );
 
-  async ngOnInit() {
-    await this.fetchStudents();
+  // all where there is only one assignment and that one is pending
+  singleAssignments = computed(() =>
+    this.studentStore.students().filter(
+      (s) =>
+        s.studentAssignments.length === 1 &&
+        s.studentAssignments[0].status === Status.Pending
+    )
+  );
+
+  // all where there is no assignment
+  noAssignments = computed(() =>
+    this.studentStore.students().filter((s) => s.studentAssignments.length === 0)
+  );
+
+  async changeAssignmentStatus(student: Student, index: number, status: Status) {
+    student.studentAssignments[index].status = status;
+    await this.studentStore.updateStudent(student);
   }
 
-  async fetchStudents(): Promise<void> {
-    this.students.set(await this.service.getStudents());
-    this.processStudents();
-  }
-
-  processStudents(): void {
-    // all where there is more than one assignment and at least one is pending
-    this.conflicts.set(
-      this.students().filter(
-        (s) =>
-          s.studentAssignments.filter((a) => a.status === Status.Pending)
-            .length >= 1 && s.studentAssignments.length > 1
-      )
-    );
-    // all where there is only one assingment and that one is pending
-    this.singleAssignments.set(
-      this.students().filter(
-        (s) =>
-          s.studentAssignments.length === 1 &&
-          s.studentAssignments[0].status === Status.Pending
-      )
-    );
-    // all where there are no assignments
-    this.noAssignments.set(
-      this.students().filter((s) => s.studentAssignments.length === 0)
-    );
-  }
-
-  approveStudent(student: Student, index: number): void {
-    student.studentAssignments[index].status = Status.Accepted;
-    this.updateStudent(student);
-  }
-
-  undoStatus(student: Student, index: number): void {
-    student.studentAssignments[index].status = Status.Pending;
-    this.updateStudent(student);
-  }
-
-  rejectStudent(student: Student, index: number): void {
-    student.studentAssignments[index].status = Status.Declined;
-    this.updateStudent(student);
-  }
-
-  async autoApproveAll(): Promise<void> {
+  async autoApproveAll(): Promise<Promise<void>> {
     this.singleAssignments().forEach((student) => {
-      const assignment = student.studentAssignments.find(
-        (a) => a.status === Status.Pending
-      );
-      if (assignment) this.approveStudent(student, 0);
+      this.changeAssignmentStatus(student, 0, Status.Accepted);
     });
-    await this.fetchStudents();
   }
 
-  async updateStudent(student: Student): Promise<void> {
-    await this.service.updateStudent(student);
-    await this.fetchStudents();
-  }
+  protected readonly Status = Status;
 }
