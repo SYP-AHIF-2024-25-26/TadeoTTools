fbc01703eddbdee4e295faad0ed0fc7df2b7ad47
HuberAndreas1 <a.huber1@students.htl-leonding.ac.at>
2025-03-29 20:20:03 +0100

    KAN-160: Added Feedback component to routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,9 +5,11 @@ import { MapComponent } from './map/map.component';
 import { StopPageComponent } from './stop-page/stop-page.component';
 import { AboutPageComponent } from './about-page/about-page.component';
 import { NextYearPageComponent } from './next-year-page/next-year-page.component';
+import { FeedbackComponent } from './feedback/feedback.component';
 
 export const routes: Routes = [
   { path: '', component: NextYearPageComponent },
+  { path: 'feedback', component: FeedbackComponent},
   /*{ path: 'main', component: MainPageComponent },
   { path: 'tour/:stopGroupId', component: StopPageComponent },
   { path: 'tour/:stopGroupId/stop/:stopId', component: StopDescriptionPageComponent },
