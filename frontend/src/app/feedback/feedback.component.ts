961af49bb8e2ce4d19358f6016e508c4d255a5be
HuberAndreas1 <a.huber1@students.htl-leonding.ac.at>
2025-03-30 13:16:06 +0200

    KAN-160: Added some functionality e.g. previous button

--- frontend/src/app/feedback/feedback.component.ts
+++ frontend/src/app/feedback/feedback.component.ts
@@ -1,14 +1,16 @@
-import { Component, computed, inject, OnInit, signal, WritableSignal } from '@angular/core';
+import { Component, inject, OnInit, signal, WritableSignal } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ApiFetchService } from '../api-fetch.service';
 import { FeedbackQuestion, FeedbackSubmission } from '../types';
 import { HeaderComponent } from '../header/header.component';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-feedback',
   imports: [
     ReactiveFormsModule,
     HeaderComponent,
+    NgStyle,
   ],
   templateUrl: './feedback.component.html',
   styleUrl: './feedback.component.css'
@@ -18,11 +20,20 @@ export class FeedbackComponent implements OnInit{
   currentQuestionIndex = 0;
   answers: string[] = [];
   form!: FormGroup;
+  progressPercentage = signal(0);
+  showPopup = signal(false);
 
   private fb: FormBuilder = inject(FormBuilder);
   private apiFetchService: ApiFetchService = inject(ApiFetchService);
 
   isLastQuestion = () => this.currentQuestionIndex === this.questions().length - 1;
+
+  updateProgressBar = () => {
+    if (this.questions().length > 0) {
+      this.progressPercentage.set(((this.currentQuestionIndex + 1) / this.questions().length) * 100);
+    }
+  }
+
   async nextOrSubmit(): Promise<void> {
     if (this.form.valid) {
       this.answers[this.currentQuestionIndex] = this.form.value.answer;
@@ -30,11 +41,14 @@ export class FeedbackComponent implements OnInit{
       if (!this.isLastQuestion()) {
         this.currentQuestionIndex++;
         this.form.reset();
+        this.updateProgressBar();
       } else {
         const feedbackSubmissions = this.answers.map((answer, index) => ({ questionId: this.questions()[index].id, answer: answer } as FeedbackSubmission));
         await this.apiFetchService.submitFeedback(feedbackSubmissions);
         this.currentQuestionIndex = 0;
         this.form.reset();
+        this.updateProgressBar();
+        this.showPopup.set(true);
       }
     }
   }
@@ -44,5 +58,15 @@ export class FeedbackComponent implements OnInit{
     this.form = this.fb.group({
       answer: ['', Validators.required]
     });
+    this.updateProgressBar();
+  }
+
+  previous(): void {
+    if (this.currentQuestionIndex > 0) {
+      this.currentQuestionIndex--;
+      // Optionally, pre-fill the answer if already provided
+      this.form.patchValue({ answer: this.answers[this.currentQuestionIndex] || '' });
+      this.updateProgressBar();
+    }
   }
 }
