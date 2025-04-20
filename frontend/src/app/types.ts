dbed2323ad9f3e85d53cae26e5606ae5cf524c9e
HuberAndreas1 <a.huber1@students.htl-leonding.ac.at>
2025-04-20 10:20:34 +0200

    KAN-160: Changed FeedbackQuestion type to new data model

--- frontend/src/app/types.ts
+++ frontend/src/app/types.ts
@@ -23,8 +23,16 @@ export type Division = {
 };
 
 export type FeedbackQuestion = {
-  id: number;
-  question: string;
+  id?: number
+  question: string
+  type: "text" | "choice" | "rating"
+  required: boolean
+  placeholder?: string
+  options?: string[]
+  minRating?: number
+  maxRating?: number
+  ratingLabels?: string,
+  order: number
 }
 
 export type FeedbackSubmission = {
