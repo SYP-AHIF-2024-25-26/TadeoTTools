c843087e2d2abf48adde14d163e2fc966aa05ccb
Melanie Dohr <m.dohr@students.htl-leonding.ac.at>
2025-09-23 20:42:09 +0200

    removed all of the info store

--- dashboard/src/app/admin.service.ts
+++ dashboard/src/app/admin.service.ts
@@ -2,8 +2,6 @@ import { inject, Injectable } from '@angular/core';
 import { BASE_URL } from './app.config';
 import { HttpClient } from '@angular/common/http';
 import { firstValueFrom } from 'rxjs';
-import { Info } from './types';
-import { InfoStore } from './store/info.store';
 
 @Injectable({
   providedIn: 'root',
@@ -11,9 +9,6 @@ import { InfoStore } from './store/info.store';
 export class AdminService {
   httpClient = inject(HttpClient);
   baseUrl = inject(BASE_URL);
-  private readonly infoStore = inject(InfoStore);
-
-  constructor() {}
 
   async addAdmin(name: string) {
     try {
@@ -22,9 +17,7 @@ export class AdminService {
           headers: { 'Content-Type': 'application/json' },
         })
       );
-      this.infoStore.addInfo({ id: 0, type: 'info', message: 'Successfully added admin' });
     } catch (error) {
-      this.infoStore.addInfo({ id: 0, type: 'error', message: 'Failed to add admin' });
       throw error;
     }
   }
@@ -32,9 +25,7 @@ export class AdminService {
   async deleteAdmin(name: string) {
     try {
       await firstValueFrom(this.httpClient.delete(this.baseUrl + `/api/admins/${name}`));
-      this.infoStore.addInfo({ id: 0, type: 'info', message: 'Successfully deleted admin' });
     } catch (error) {
-      this.infoStore.addInfo({ id: 0, type: 'error', message: 'Failed to delete admin' });
       throw error;
     }
   }
@@ -43,7 +34,6 @@ export class AdminService {
     try {
       return await firstValueFrom(this.httpClient.get<string[]>(this.baseUrl + '/api/admins'));
     } catch (error) {
-      this.infoStore.addInfo({ id: 0, type: 'error', message: 'Failed to get admins' });
       throw error;
     }
   }
--- dashboard/src/app/teacher.service.ts
+++ dashboard/src/app/teacher.service.ts
@@ -1,75 +1,60 @@
 import { inject, Injectable } from '@angular/core';
-import { Teacher, TeacherAssignment, Info } from './types';
-import { async, firstValueFrom } from 'rxjs';
+import { Teacher, TeacherAssignment } from './types';
+import { firstValueFrom } from 'rxjs';
 import { BASE_URL } from './app.config';
 import { HttpClient } from '@angular/common/http';
-import { InfoStore } from './store/info.store';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TeacherService {
-  baseUrl = inject(BASE_URL);
-  httpClient = inject(HttpClient);
-  private readonly infoStore = inject(InfoStore);
+  private baseUrl = inject(BASE_URL);
+  private httpClient = inject(HttpClient);
 
-  constructor() {}
-
-  async getTeachers() {
-    try {
-      return await firstValueFrom(this.httpClient.get<Teacher[]>(this.baseUrl + '/api/teachers'));
-    } catch (error) {
-      this.infoStore.addInfo({ id: 0, type: 'error', message: 'Failed to get teachers' });
-      throw error;
-    }
+  getTeachers() {
+    return firstValueFrom(
+      this.httpClient.get<Teacher[]>(`${this.baseUrl}/api/teachers`)
+    );
   }
 
-  async postTeacher(teacher: Teacher) {
-    try {
-      await firstValueFrom(this.httpClient.post<void>(this.baseUrl + '/api/teachers', teacher));
-    } catch (error) {
-      this.infoStore.addInfo({ id: 0, type: 'error', message: 'Failed to post teacher' });
-      throw error;
-    }
+  postTeacher(teacher: Teacher) {
+    return firstValueFrom(
+      this.httpClient.post<void>(`${this.baseUrl}/api/teachers`, teacher)
+    );
   }
 
-  async deleteTeacher(edufsUsername: string) {
-    try {
-      await firstValueFrom(this.httpClient.delete<void>(this.baseUrl + '/api/teachers/' + edufsUsername));
-    } catch (error) {
-      this.infoStore.addInfo({ id: 0, type: 'error', message: 'Failed to delete teacher' });
-      throw error;
-    }
+  deleteTeacher(edufsUsername: string) {
+    return firstValueFrom(
+      this.httpClient.delete<void>(`${this.baseUrl}/api/teachers/${edufsUsername}`)
+    );
   }
 
-  async updateTeacher(teacher: Teacher) {
-    try {
-      await firstValueFrom(this.httpClient.put<void>(this.baseUrl + '/api/teachers', teacher));
-    } catch (error) {
-      this.infoStore.addInfo({ id: 0, type: 'error', message: 'Failed to update teacher' });
-      throw error;
-    }
+  updateTeacher(teacher: Teacher) {
+    return firstValueFrom(
+      this.httpClient.put<void>(`${this.baseUrl}/api/teachers`, teacher)
+    );
   }
 
-  async setAssignments(edufsUsername: string, assignments: number[]): Promise<void> {
-    try {
-      await firstValueFrom(this.httpClient.put<void>(this.baseUrl + '/api/teachers/' + edufsUsername + '/assignments', assignments.map(a => ({stopId: a, teacherId: edufsUsername} as TeacherAssignment))));
-    } catch (error) {
-      this.infoStore.addInfo({ id: 0, type: 'error', message: 'Failed to set teacher assignments' });
-      throw error;
-    }
+  setAssignments(edufsUsername: string, assignments: number[]) {
+    const teacherAssignments = assignments.map(stopId => ({
+      stopId,
+      teacherId: edufsUsername
+    } as TeacherAssignment));
+
+    return firstValueFrom(
+      this.httpClient.put<void>(
+        `${this.baseUrl}/api/teachers/${edufsUsername}/assignments`,
+        teacherAssignments
+      )
+    );
   }
 
-  async uploadTeachersCsv(file: File) {
-    try {
-      const formData = new FormData();
-      formData.append('file', file);
+  uploadTeachersCsv(file: File) {
+    const formData = new FormData();
+    formData.append('file', file);
 
-      await firstValueFrom(this.httpClient.post<void>(this.baseUrl + '/api/teachers/upload', formData));
-      this.infoStore.addInfo({ id: 0, type: 'info', message: 'Successfully uploaded students CSV' });
-    } catch (error) {
-      this.infoStore.addInfo({ id: 0, type: 'error', message: 'Failed to upload students CSV' });
-      throw error;
-    }
+    return firstValueFrom(
+      this.httpClient.post<void>(`${this.baseUrl}/api/teachers/upload`, formData)
+    );
   }
 }
