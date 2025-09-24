8b01e0c685faf9ca422e82d7b202c08d0a52fd57
Melanie Dohr <m.dohr@students.htl-leonding.ac.at>
2025-09-24 07:34:37 +0200

    refactored admin service

--- dashboard/src/app/admin.service.ts
+++ dashboard/src/app/admin.service.ts
@@ -7,34 +7,30 @@ import { firstValueFrom } from 'rxjs';
   providedIn: 'root',
 })
 export class AdminService {
-  httpClient = inject(HttpClient);
-  baseUrl = inject(BASE_URL);
+  private httpClient = inject(HttpClient);
+  private baseUrl = inject(BASE_URL);
 
-  async addAdmin(name: string) {
-    try {
-      await firstValueFrom(
-        this.httpClient.post(`${this.baseUrl}/api/admins?name=${encodeURIComponent(name)}`, null, {
+  addAdmin(name: string): Promise<void> {
+    return firstValueFrom(
+      this.httpClient.post<void>(
+        `${this.baseUrl}/api/admins?name=${encodeURIComponent(name)}`,
+        null,
+        {
           headers: { 'Content-Type': 'application/json' },
-        })
-      );
-    } catch (error) {
-      throw error;
-    }
+        }
+      )
+    );
   }
 
-  async deleteAdmin(name: string) {
-    try {
-      await firstValueFrom(this.httpClient.delete(this.baseUrl + `/api/admins/${name}`));
-    } catch (error) {
-      throw error;
-    }
+  deleteAdmin(name: string): Promise<void> {
+    return firstValueFrom(
+      this.httpClient.delete<void>(`${this.baseUrl}/api/admins/${name}`)
+    );
   }
 
-  async getAdmins(): Promise<string[]> {
-    try {
-      return await firstValueFrom(this.httpClient.get<string[]>(this.baseUrl + '/api/admins'));
-    } catch (error) {
-      throw error;
-    }
+  getAdmins(): Promise<string[]> {
+    return firstValueFrom(
+      this.httpClient.get<string[]>(`${this.baseUrl}/api/admins`)
+    );
   }
 }
