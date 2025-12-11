cb70a586510db9f319e4822d90530c895897901c
Melanie Dohr <m.dohr@students.htl-leonding.ac.at>
2025-12-11 12:00:28 +0100

    apphost to dotnet 10 + other package for node

--- backend/AppHost/AppHost.cs
+++ backend/AppHost/AppHost.cs
@@ -8,17 +8,15 @@ var db = pgsql.AddDatabase("tadeot");
 
 // API project orchestrated by Aspire
 var webapi = builder.AddProject<Projects.API>("api")
-    // Map the database connection into API's ConnectionStrings:DefaultConnection
     .WithReference(db, connectionName: "DefaultConnection")
-    // Make HTTP endpoints accessible from host machine
     .WithExternalHttpEndpoints();
 
-var dashboard = builder.AddNpmApp("dashboard", "../../dashboard")
+var dashboard = builder.AddJavaScriptApp("dashboard", "../../dashboard")
     .WithReference(webapi)
     .WithHttpEndpoint(env: "PORT")
     .WithExternalHttpEndpoints();
 
-var guideapp = builder.AddNpmApp("frontend", "../../frontend")
+var guideapp = builder.AddJavaScriptApp("frontend", "../../frontend")
     .WithReference(webapi)
     .WithHttpEndpoint(env: "PORT")
     .WithExternalHttpEndpoints();
