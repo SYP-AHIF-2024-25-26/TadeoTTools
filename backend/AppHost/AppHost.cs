1c8d489bcd2eb646977e568dd1bb7d92013086ca
Melanie Dohr <m.dohr@students.htl-leonding.ac.at>
2025-12-17 22:11:51 +0100

    fixed postgres bug

--- backend/AppHost/AppHost.cs
+++ backend/AppHost/AppHost.cs
@@ -1,14 +1,14 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 // PostgreSQL server and database for the application
+/*
 var pgsql = builder.AddPostgres("postgres")
     .WithDataVolume();
-
-var db = pgsql.AddDatabase("tadeot");
+*/
 
 // API project orchestrated by Aspire
+// API project orchestrated by Aspire
 var webapi = builder.AddProject<Projects.API>("api")
-    .WithReference(db, connectionName: "DefaultConnection")
     .WithExternalHttpEndpoints();
 
 var dashboard = builder.AddJavaScriptApp("dashboard", "../../dashboard")
